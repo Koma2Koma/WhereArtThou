a444f582c76282bb8baf8473a4c29f06bf2135fb
Amanda Walter <anhart@gmail.com>
2015-01-13 08:59:58 -0700

    Add route to root page

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
-  root 'welcome#index'
+  # root 'welcome#index'
+  root 'static_pages/home'
 
   devise_for :users, :controllers => {registrations: 'registrations'}
 
   resources :users, only: [:show, :index]
 
+end
