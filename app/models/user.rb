199647173dda0cceb523b7bb77c936fdfaa3cb49
Craig10n <nielson.craige@gmail.com.com>
2015-01-15 11:49:28 -0700

    creatingstaticview

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  belongs_to :category
+  has_attached_file :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
