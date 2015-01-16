5dd07e2ea86e404a731ada7146cbdf5ecc078240
annemnichols <anne@seantnichols.com>
2015-01-16 14:59:36 -0700

    Format registrations form to accept is_artist boolean

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,13 +4,13 @@ class RegistrationsController < Devise::RegistrationsController
 
 	def sign_up_params
 		params.require(:user).permit(:username, :email, 
-																 :password, :password_confirmation)
+																 :password, :password_confirmation, :is_artist)
 	end
 
 	def account_update_params
 		params.require(:user).permit(:username, :email, 
 																 :password, :password_confirmation, 
-																 :current_password)
+																 :current_password, :is_artist)
 	end
 
 end
