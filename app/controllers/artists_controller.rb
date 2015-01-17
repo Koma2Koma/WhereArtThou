1d9fc5f4bda590ae8e28ff7b1cd57293c998dd11
Amanda Walter <anhart@gmail.com>
2015-01-16 21:31:18 -0700

    Add AWS gem

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,4 +6,17 @@ class ArtistsController < ApplicationController
   def show
   end
 
+  def new
+  end
+
+  def create
+  	@artist = Artist.new(artist_params)
+  	@artist.user_id = current_user.user_id
+  	if @artist.save
+  		redirect
+  	else
+  		render :new
+  	end
+  end
+
 end
