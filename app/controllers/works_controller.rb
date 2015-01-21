7fb17748f5b4f24c7c72d75a8863fd097f83e4a0
Craig10n <nielson.craige@gmail.com.com>
2015-01-21 14:56:43 -0700

    format mission page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,9 +2,10 @@ class WorksController < ApplicationController
 
   def index
 
-    search_params = params[:search].downcase
+
 
     if search_params
+      search_params = params[:search].downcase
       @works = Work.where('lower(title) LIKE ?', "%#{search_params}%")
       @artists = User.where('lower(username) LIKE ? AND is_artist = ?', "%#{search_params}%", true)
       @users = User.where('lower(username) LIKE ? AND is_artist = ?', "%#{search_params}%", false)
