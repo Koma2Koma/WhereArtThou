f6ac9e57f3d03d354a98151b3799eebed8c05f9f
Konrad Maurer <kradjr@gmail.com>
2015-01-21 17:45:14 -0700

    updates models with likeabilities.

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,8 @@
 class Work < ActiveRecord::Base
   belongs_to :artist
 
+  acts_as_likeable
+
   has_attached_file :image, :styles => { :small => "200x200>", :medium => "300x300>", :large => "450x450>", :thumb => "150x150>" }, :default_url => "/images/:style/missing.png"
   validates_attachment :image, :presence => true,
     :content_type => { :content_type => /\Aimage\/.*\Z/ },
