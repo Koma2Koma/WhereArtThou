84ac7d54c1e7fd15f19872f44cf9dfb4ee913366
Konrad Maurer <kradjr@gmail.com>
2015-01-24 14:53:43 -0700

    discover page map loads at users current location.

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,20 +1,49 @@
 $(function(){
-  handler = Gmaps.build('Google');
-  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-    markers = handler.addMarkers([
-      {
-        "lat": 40.7648919,
-        "lng": 111.8853819,
-        "picture": {
-          "url": "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
-          "width":  36,
-          "height": 36
-        },
-        "infowindow": "hello!"
-      }
-    ]);
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
-  });
+
+    var lat = 0;
+    var lon = 0;
+    var latLon = [];
+
+   function showPosition(position) {
+      lon = position.coords.longitude;
+      latLon = [lat, lon]
+      console.log( latLon );
+      callMap();
+  };
+  // var x = document.getElementById("demo");
+  function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, error);
+      } else {
+        $( '#demo').html( "Geolocation is not supported by this browser." );
+      };
+  };
+
+  function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  };
+
+  getLocation();
+  var lAndl;
+
+  function callMap() {
+    handler = Gmaps.build('Google');
+    handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+      markers = handler.addMarkers([
+        {
+          "lat": latLon[0],
+          "lng": latLon[1],
+          "picture": {
+            "url": "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
+            "width":  36,
+            "height": 36
+          },
+          "infowindow": "hello!"
+        }
+      ]);
+      handler.bounds.extendWith(markers);
+      handler.fitMapToBounds();
+    });
+  };
   
 });
