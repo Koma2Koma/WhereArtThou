57da8f1e450688ae9fff276995e51e9d42890145
Konrad Maurer <kradjr@gmail.com>
2015-01-24 13:37:13 -0700

    finshed learning how to use example.

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,10 +1,13 @@
 class Venue < ActiveRecord::Base
+  validates :address, :city, :state, presence: true
+
   geocoded_by :full_street_address
+  after_validation :geocode
 
   reverse_geocoded_by :latitude, :longitude
 
   def full_street_address
-    self.address << ', ' + self.city << ', ' + self.state
+    [address, city, state].join(', ')
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124173442) do
+ActiveRecord::Schema.define(version: 20150124173858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,6 +134,8 @@ ActiveRecord::Schema.define(version: 20150124173442) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "contact"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "works", force: true do |t|
