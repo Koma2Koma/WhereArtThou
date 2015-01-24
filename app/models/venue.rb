f8a0eec15540398137a6678e0283bf1c1c0a80c2
Konrad Maurer <kradjr@gmail.com>
2015-01-24 11:31:41 -0700

    small progress.

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,4 +2,9 @@ class Venue < ActiveRecord::Base
   geocoded_by :full_street_address
 
   reverse_geocoded_by :latitude, :longitude
+
+  def full_street_address
+    self.address << ', ' + self.city << ', ' + self.state
+  end
+
 end
