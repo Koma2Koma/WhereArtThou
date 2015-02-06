857ae47f7815a2dafc9e185c322fbc7040a3a4b3
Craig10n <nielson.craige@gmail.com.com>
2015-02-06 14:53:10 -0700

    continuing to work on about page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206074430) do
+ActiveRecord::Schema.define(version: 20150206211905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,14 @@ ActiveRecord::Schema.define(version: 20150206074430) do
   add_index "likes", ["likeable_id", "likeable_type"], name: "fk_likeables", using: :btree
   add_index "likes", ["liker_id", "liker_type"], name: "fk_likes", using: :btree
 
+  create_table "pg_search_documents", force: true do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
   create_table "taggings", force: true do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
@@ -153,9 +161,9 @@ ActiveRecord::Schema.define(version: 20150206074430) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "contact"
-    t.integer  "user_id"
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "user_id"
     t.string   "picture_file_name"
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
