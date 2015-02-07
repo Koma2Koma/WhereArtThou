2e197375960006a9129d37b2ff1c8d5b86bf9366
Amanda Walter <anhart@gmail.com>
2015-02-06 17:37:28 -0700

    Fix empty migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206074430) do
+ActiveRecord::Schema.define(version: 20150206211905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -153,9 +153,9 @@ ActiveRecord::Schema.define(version: 20150206074430) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "contact"
-    t.integer  "user_id"
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "user_id"
     t.string   "picture_file_name"
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
