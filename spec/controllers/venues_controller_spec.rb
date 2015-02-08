f5d72cb6bdb75d5008e6223e032425e9df40ea73
Konrad Maurer <kradjr@gmail.com>
2015-02-08 12:08:06 -0700

    Adds get index specs to the venue controller spec.

--- spec/controllers/venues_controller_spec.rb
+++ spec/controllers/venues_controller_spec.rb
@@ -1,13 +1,27 @@
 require 'rails_helper'
 
 describe VenuesController do
+
   describe 'GET #index' do
-    it 'populates an array of all venues'
-    it 'renders the :index view'
+
+    it 'populates an array of all venues' do
+      venue = FactoryGirl.create(:venue)
+      get :index
+      expect(assigns(:venues)).to eq([venue])
+    end
+
+    it 'renders the :index view' do
+      get :index
+      expect(response).to render_template :index
+    end
   end
 
   describe 'GET #show' do
-    it 'creates an instance variable @venue with the desired venue'
+    it 'creates an instance variable @venue with the desired venue' do
+      venue = FactoryGirl.create(:venue)
+
+    end
+
     it 'renders the :show view'
   end
 
